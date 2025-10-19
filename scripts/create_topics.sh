5da0d78920f32117e2d74a1bc0c85b93a1d1d786
Paweł Kawula <pawel.kawula@proton.me>
2025-10-19 18:18:24 +0200

    fix: sleep less after docker compose but sleep after creating topic

--- scripts/create_topics.sh
+++ scripts/create_topics.sh
@@ -39,6 +39,7 @@ if [ -n "$KAFKA_TOPIC_LIST" ]; then
   DOCKER_COMMAND+="\" "
   info "Attempting to create kafka topics ..."
   eval $DOCKER_COMMAND
+  sleep 10
 else
   info "No kafka topics to create."
 fi
